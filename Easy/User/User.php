99e29336c9c33d515485798fabc02074d79dce2e
kruemel <soeren@siegismund-poschmann.de>
2013-10-01 12:30:01 +0200

    User

---
+++ Easy/User/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace Easy\User;
+
+class User
+{
+	public function __construct($pdo)
+	{
+		$this->pdo = $pdo;
+	}
+	public function register($mail, $pw, $hash = false;)
+	{
+		$sql = "INSERT INTO `user`(`id`, `mail`, `pw`) VALUES (NULL, ?, ?)";
+		$this->pdo->insert($sql, array($mail, md5($pw)));
+	}
+	public function checkHash($mail, $hash)
+	{
+
+	}
+	public function login($mail, $pw)
+	{
+		$sql = 'SELECT `id`, `pw` FROM `user` WHERE `mail` == "?"';
+		$res = $this->pdo->query($sql, array($mail));
+		if(count($res)!=1)
+		{
+			return False;
+		}
+		if($res["pw"]==md5($pw))
+		{
+			return True;
+		}
+		return False;
+	}
+}
+?>
